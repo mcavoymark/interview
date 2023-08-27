b52c368c48f1d21564599a9b2db53c4ddb4c22c7
Mark McAvoy <106771462+mcavoymark@users.noreply.github.com>
2023-08-27 14:19:07 -0500

    Add files via upload

---
+++ int2roman_230827/int2roman_230827/int2roman_230827.cpp
@@ -0,0 +1,49 @@
+﻿// int2roman_230827.cpp : Defines the entry point for the application.
+//
+
+/*
+#I      1
+#V      5
+#X      10
+#L      50
+#C      100
+#D      500
+#M      1000
+*/
+
+#include "int2roman_230827.h"
+
+class Int2rom {
+	public:
+		std::string int2rom(int num);
+};
+
+std::string Int2rom::int2rom(int num) {
+	std::vector<std::string> rom = { "I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M" };
+	std::vector<int> cnt = {          1,   4,   5,   9, 10, 40, 50,  90, 100,400, 500,900, 1000 };
+
+	std::string str;
+	for (int i = rom.size() - 1; num > 0; --i) {
+		for (int j = 0; j < num / cnt[i]; ++j) str += rom[i];
+		num %= cnt[i];
+	}
+	return str;
+};
+
+//using namespace std;
+
+int main()
+{
+	Int2rom ir;
+
+	//int num = 3549;
+	//std::cout << num << " is " << ir.int2rom(num) << std::endl;
+
+	std::string str;
+	while (std::getline(std::cin, str)) {
+		int num = std::stoi(str);
+		std::cout << num << " is " << ir.int2rom(num) << std::endl;
+	}
+
+	return 0;
+}
---
+++ int2roman_230827/int2roman_230827/int2roman_230827.h
@@ -0,0 +1,10 @@
+﻿// int2roman_230827.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+// TODO: Reference additional headers your program requires here.
